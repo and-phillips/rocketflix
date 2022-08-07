d07db373e9016b4c83aa7847582de266cf15d4d0
Anderson Phillips <anderson.phillips95@gmail.com>
2022-08-07 19:07:19 -0300

    :tada: first commit

---
+++ api.js
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = '6bd3911da01d0b5a7f7fadd94f9374f4';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
---
+++ index.js
@@ -0,0 +1,47 @@
+import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
+
+const shuffleButton = document.getElementById("shuffleButton");
+shuffleButton.addEventListener("click", getMovie);
+
+function getMovie() {
+  let movieId = Math.floor(Math.random() * 1000 + 1);
+  const container = document.querySelector(".container");
+  const url = `${BASE_URL}${movieId}?api_key=${API_KEY}&${language}`;
+
+  axios.get(url)
+    .then((response) => {
+      const data = response.data;
+      let movieTitle = data.title;
+      let movieOverview;
+      let poster = `${IMG_URL}` + data.poster_path;
+      let rate = data.vote_average;
+
+      if (data.overview === "") {
+        movieOverview =
+          "Desculpe. Não temos uma sinopse em Português deste filme.";
+      } else {
+        movieOverview = data.overview;
+      }
+
+      container.innerHTML = `
+        <div class="movie-poster">
+          <img src="${poster}" alt="movie-poster" id="poster">
+          <p>Nota média: ${rate}</p>
+        </div>
+        <div class="movie-infos">
+          <h3 id="titleMovie">${movieTitle}</h3>
+          <p id="movieOverview">${movieOverview}</p>
+        </div>
+      `;
+    })
+    .catch(() => {
+      container.innerHTML = `
+        <div class="movie-poster">
+          <img src="./assets/poster.png" alt="movie-poster" id="poster">
+        </div>
+        <div class="movie-infos coding">
+          <p class="noFilm">Ops, hoje não é dia de assistir filme. Bora codar! 🚀</p>
+        </div>
+      `;
+    });
+}
